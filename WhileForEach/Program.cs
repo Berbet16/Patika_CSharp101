a82627a000927e38373cf71813a8073d86a9bbca
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-07 14:30:25 +0300

    Add files via upload

---
+++ WhileForEach/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace WhileForeachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+
+            // 1 den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a dan z ye kadar tüm harfleri console a yazdır.
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //Foreach
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+using System;
+
+namespace WhileForeachLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+
+            // 1 den başlayarak console'dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+            // a dan z ye kadar tüm harfleri console a yazdır.
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //Foreach
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
