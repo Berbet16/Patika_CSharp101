96115ac7653b1620b9ff175db5c526a7238eb5db
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-04-07 22:29:34 +0300

    Add files via upload

---
+++ SınıfNedir/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Calisan calisan1 = new Calisan();
+            calisan1.Ad = "Berna";
+            calisan1.Soyad = "Soylu";
+            calisan1.No = 107;
+            calisan1.Departman = "Yazılım";
+
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("*************");
+            Calisan calisan2 = new Calisan();
+            calisan2.Ad = "Betül";
+            calisan2.Soyad = "Soylu";
+            calisan2.No = 8531;
+            calisan2.Departman = "Eğitim";
+
+            calisan2.CalisanBilgileri();
+
+        }
+        class Calisan
+        {
+            //Property : Özellikler
+            public string Ad;
+            public string Soyad;
+            public int No;
+            public string Departman;
+            public void CalisanBilgileri()
+            {
+                Console.WriteLine("Çalışan Adı : {0}", Ad);
+                Console.WriteLine("Çalışan Soyad : {0}", Soyad);
+                Console.WriteLine("Çalışan No : {0}", No);
+                Console.WriteLine("Çalışan Departman : {0}", Departman);
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
